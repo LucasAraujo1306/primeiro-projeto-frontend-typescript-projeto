c68ea6f855847197cff538e8e9c53c6ac289127d
LucasAraujo1306 <lucasisaac007@gmail.com>
2023-06-10 22:02:44 -0300

    integração do sign page com api

--- base/src/pages/SignIn/index.tsx
+++ base/src/pages/SignIn/index.tsx
@@ -1,17 +1,53 @@
 import './styles.css'
-
 import { Link } from 'react-router-dom'
+import api from '../../services/api'
+import { FormEvent, useState } from 'react';
+
 
 function SignIn(){
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  async function handleSubmit(event:FormEvent){
+    event.preventDefault();
+
+    try {
+      if(!email || !password){
+        throw new Error ('Preencha todos os campos');
+      }
+      const response = await api.post('/login', {
+        email,
+        password
+      })
+      console.log(response );
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
+
   return(
     <div className='container container-sign-in'>
       <div className='sign-in'>
       <img src='' alt="logo" />
 
-      <form>
-        <input type="text" placeholder='E-mail' />
-        <input type="password" placeholder='Senha' />
-
+      <form onSubmit={handleSubmit}>
+        <input 
+        type="text" 
+        placeholder='E-mail' 
+        value={email}
+        onChange={(e)=>setEmail(e.target.value)}
+        required
+        />
+        <input 
+        type="password" 
+        placeholder='Senha' 
+        value={password}
+        onChange={(e)=>setPassword(e.target.value)}
+        required
+        />
+  
         <span>Não tem cadastro ? 
           <Link to="/signup">Clique aqui!</Link>
         </span>
